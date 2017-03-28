ed6af6d554e559e4f8a7030d08c6e96270e568e6
AoranZhao <aoran_zhao@yahoo.ca>
2017-03-28 18:19:27 -0400

    renew touch move

--- src/Components/ScrollView.js
+++ src/Components/ScrollView.js
@@ -113,6 +113,8 @@ export default class ScrollView extends Component {
 	}
 
 	handleTouchMove(e) {
+		var dst = e.touches[0].clientX - this.state.t_s_x;
+		this.props.trigger_offset(dst / 50);
 		this.setState({
 			t_x: e.touches[0].clientX,
 			t_y: e.touches[0].clientY
